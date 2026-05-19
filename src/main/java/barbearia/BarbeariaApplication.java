d441b60c5d60e821e05119c4b3be521e9f8ff1c7
Rafael <rp032465@gmail.com>
2026-05-19 20:22:08 -0300

    fix: ajustando SpringBootApplication para permitir varredura completa e excluir seguranca padrao

--- src/main/java/barbearia/BarbeariaApplication.java
+++ src/main/java/barbearia/BarbeariaApplication.java
@@ -3,9 +3,15 @@ package barbearia;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "barbearia") // <--- ISSO AQUI É A CHAVE
+// ❌ Removemos o scanBasePackages travado e desativamos a autoconfiguração de segurança
+@SpringBootApplication(exclude = { 
+    SecurityAutoConfiguration.class, 
+    UserDetailsServiceAutoConfiguration.class 
+})
 @EnableJpaRepositories(basePackages = "barbearia.repository")
 @EntityScan(basePackages = "barbearia.entity")
 public class BarbeariaApplication {
